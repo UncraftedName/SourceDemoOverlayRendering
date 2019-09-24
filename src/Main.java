5f6ff68c0274b760862084d146ffb24e5d01eea2
UncraftedName <uncraftedname@gmail.com>
2019-09-24 00:09:56 -0700

    added tga to png conversion

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import processing.core.PApplet;
 public class Main extends PApplet {
 
     public static void main(String[] args) {
-        PApplet.main("Main");
+        PApplet.main(Main.class);
     }
 
 
